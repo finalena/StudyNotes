c9e9f58800551b43dd480e27aaee03f628666503
finalena <islenaliny@gmail.com>
2021-07-20 23:35:46 +0800

    重構Find, Update, Delete

--- C_sharp/13_三層式架構/Ex_13_10/MultiLayers/MemberUI.cs
+++ C_sharp/13_三層式架構/Ex_13_10/MultiLayers/MemberUI.cs
@@ -10,7 +10,6 @@ namespace MultiLayers
 {
 	class MemberUI
 	{
-		private string fileName = @"C:\Users\USER\source\repos\StudyNotes\C_sharp\13_三層式架構\Ex_13_10\members.txt";
 		private MemberRepository memberRepo;
 
 		[SetUp]
@@ -29,11 +28,6 @@ namespace MultiLayers
 				Name = name
 			};
 			memberRepo.Create(member);
-			//string recode = $"{id}.{ name }";
-			//using (StreamWriter sw = File.AppendText(fileName))
-			//{
-			//	sw.WriteLine(recode);
-			//}
 		}
 
 		[Test]
@@ -46,33 +40,11 @@ namespace MultiLayers
 			});
 		}
 
-		//private List<Member> GetAll()
-		//{
-		//	return memberRepo.GetAll();
-		//	//var data = File.ReadAllText(this.fileName);
-		//	//string[] rows = data.Split('\n');
-		//	//List<Member> result = new List<Member>();
-		//	//foreach (var row in rows)
-		//	//{
-		//	//	if (string.IsNullOrEmpty(row)) continue;
-
-		//	//	string[] items = row.Split('.');
-		//	//	Member member = new Member
-		//	//	{
-		//	//		Id = Convert.ToInt32(items[0]),
-		//	//		Name = items[1]
-		//	//	};
-		//	//	result.Add(member);
-		//	//}
-
-		//	//return result;
-		//}
-
 		[Test]
 		public void GetMember()
 		{
 			int memberId = 1;
-			var result = memberRepo.GetAll().Single(x => x.Id == memberId);
+			var result = memberRepo.Find(memberId);
 
 			Console.WriteLine($"{result.Id}, {result.Name}");
 		}
@@ -82,37 +54,15 @@ namespace MultiLayers
 		{
 			int memberId = 1;
 			string name = "Aduery Tsai";
-			var members = GetAll();
-			var member = members.Single(x => x.Id == memberId);
-			member.Name = name;
-
-			Save(members);
-		}
-
-		private void Save(List<Member> members)
-		{
-			StringBuilder sb = new StringBuilder();
-			foreach (var member in members)
-			{
-				string row = $"{member.Id}.{member.Name}";
-				sb.AppendLine(row);
-			}
-
-			using (StreamWriter sw = File.CreateText(this.fileName))
-			{
-				sw.Write(sb.ToString());
-			}
+			Member member = new Member { Id = memberId, Name = name };
+			memberRepo.Update(member);
 		}
 
 		[Test]
 		public void DeleteMember()
 		{
 			int memberId = 2;
-			var members = GetAll();
-			var member = members.Single(x => x.Id == memberId);
-			members.Remove(member);
-
-			Save(members);
+			memberRepo.Delete(memberId);
 		}
 	}
 
@@ -148,6 +98,44 @@ namespace MultiLayers
 			}
 
 			return result;
+		}
+
+		public Member Find(int memberId)
+		{
+			return GetAll().Single(x => x.Id == memberId);
+		}
+
+		public void Update(Member member)
+		{
+			var members = GetAll();
+			var memberInDB = members.Single(x => x.Id == member.Id);
+			memberInDB.Name = member.Name;
+
+			Save(members);
+		}
+
+		private void Save(List<Member> members)
+		{
+			StringBuilder sb = new StringBuilder();
+			foreach (var member in members)
+			{
+				string row = $"{member.Id}.{member.Name}";
+				sb.AppendLine(row);
+			}
+
+			using (StreamWriter sw = File.CreateText(this.fileName))
+			{
+				sw.Write(sb.ToString());
+			}
+		}
+
+		public void Delete(int memberId)
+		{
+			var members = GetAll();
+			var member = members.Single(x => x.Id == memberId);
+			members.Remove(member);
+
+			Save(members);
 		}
 	}
 
