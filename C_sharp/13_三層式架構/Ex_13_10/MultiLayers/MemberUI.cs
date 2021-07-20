3aab7ef71b33ac18e68f34700075bce2cf3e41cc
finalena <islenaliny@gmail.com>
2021-07-20 21:45:59 +0800

    建立專案及加入Unit套件

---
+++ C_sharp/13_三層式架構/Ex_13_10/MultiLayers/MemberUI.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiLayers
+{
+	class MemberUI
+	{
+		private string fileName = Environment.CurrentDirectory;
+		[Test]
+		public void CreateMember()
+		{
+
+		}
+	}
+}
