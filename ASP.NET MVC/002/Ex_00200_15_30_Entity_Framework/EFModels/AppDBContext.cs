0934c2a590f867c1f973e47385285bb0a9963f46
finalena <islenaliny@gmail.com>
2021-07-25 22:58:39 +0800

    建立DbContext

---
+++ ASP.NET MVC/002/Ex_00200_15_30_Entity_Framework/EFModels/AppDBContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Ex_00200_15_30_Entity_Framework.EFModels
+{
+	public partial class AppDBContext : DbContext
+	{
+		public AppDBContext() : base("name=dbConn")
+		{
+
+		}
+
+		public virtual DbSet<Category> Categories { get; set; }
+		public virtual DbSet<Product> Products { get; set; }
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Category>()
+				.HasMany(e => e.Products)
+				.WithRequired(e => e.Category)
+				.WillCascadeOnDelete(false);
+		}
+	}
+}
