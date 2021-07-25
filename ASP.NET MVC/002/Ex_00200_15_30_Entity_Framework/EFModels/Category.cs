87ad23a38d012d74e2008c37ae3b7f384331d44c
finalena <islenaliny@gmail.com>
2021-07-25 22:42:57 +0800

    加入EFModels

---
+++ ASP.NET MVC/002/Ex_00200_15_30_Entity_Framework/EFModels/Category.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
+namespace Ex_00200_15_30_Entity_Framework.EFModels
+{
+	public partial class Category
+	{
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+		public Category()
+		{
+			Products = new HashSet<Product>();
+		}
+
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		public string CategoryName { get; set; }
+
+		public int DisplayOrder { get; set; }
+
+		public bool Enabled { get; set; }
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+		public virtual ICollection<Product> Products { get; set; }
+	}
+}
---
+++ ASP.NET MVC/002/Ex_00200_15_30_Entity_Framework/EFModels/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
+namespace Ex_00200_15_30_Entity_Framework.EFModels
+{
+	public partial class Product
+	{
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		public string ProductName { get; set; }
+
+		public int CategoryId { get; set; }
+
+		public int UnitPrice { get; set; }
+
+		public bool Enabled { get; set; }
+
+		public virtual Category Category { get; set; }
+	}
+}
