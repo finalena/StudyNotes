e30d138c4b1cdad4772e874d1c55fce0ca6089a5
finalena <islenaliny@gmail.com>
2021-07-25 23:24:00 +0800

    建立CategoryService.Create,Delete

--- ASP.NET MVC/002/Ex_00200_15_30_EF/Program.cs
+++ ASP.NET MVC/002/Ex_00200_15_30_EF/Program.cs
@@ -13,4 +13,41 @@ namespace Ex_00200_15_30_EF
 		{
 		}
 	}
+
+	class CategoryService
+	{
+		private AppDBContext db;
+		public CategoryService()
+		{
+			db = new AppDBContext();
+		}
+
+		/// <summary>
+		/// 新增一筆Category 記錄
+		/// </summary>
+		/// <param name="categoryName"></param>
+		/// <param name="displayOrder"></param>
+		/// <param name="enbled"></param>
+		public void Create(string categoryName, int displayOrder , bool enabled)
+		{
+			var entity = new Category
+			{
+				CategoryName = categoryName,
+				DisplayOrder = displayOrder,
+				Enabled = enabled
+			};
+
+			db.Categories.Add(entity);
+			db.SaveChanges();
+		}
+
+		public void Delete(int id)
+		{
+			var entity = db.Categories.Find(id);
+			if (entity == null) return;
+
+			db.Categories.Remove(entity);
+			db.SaveChanges();
+		}
+	}
 }
