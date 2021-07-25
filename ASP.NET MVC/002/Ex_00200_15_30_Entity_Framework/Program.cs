66cb3a2c478d1a1e37d250e65462b4cdcdbde7c4
finalena <islenaliny@gmail.com>
2021-07-25 22:42:15 +0800

    加入專案檔案及.gitignore

---
+++ ASP.NET MVC/002/Ex_00200_15_30_Entity_Framework/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_00200_15_30_Entity_Framework
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
