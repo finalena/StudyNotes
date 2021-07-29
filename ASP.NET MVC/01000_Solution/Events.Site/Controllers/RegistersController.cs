2cc045a022d0a587d259876809e77a9e3e6f4b8c
finalena <islenaliny@gmail.com>
2021-07-29 13:59:58 +0800

    modify RegistersController, remove Edit, Delete actions

--- ASP.NET MVC/01000_Solution/Events.Site/Controllers/RegistersController.cs
+++ ASP.NET MVC/01000_Solution/Events.Site/Controllers/RegistersController.cs
@@ -58,63 +58,6 @@ namespace Events.Site.Controllers
             return View(register);
         }
 
-        // GET: Registers/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Register register = db.Registers.Find(id);
-            if (register == null)
-            {
-                return HttpNotFound();
-            }
-            return View(register);
-        }
-
-        // POST: Registers/Edit/5
-        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
-        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "id,Name,Email,CreateTime")] Register register)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(register).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(register);
-        }
-
-        // GET: Registers/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Register register = db.Registers.Find(id);
-            if (register == null)
-            {
-                return HttpNotFound();
-            }
-            return View(register);
-        }
-
-        // POST: Registers/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Register register = db.Registers.Find(id);
-            db.Registers.Remove(register);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
         protected override void Dispose(bool disposing)
         {
             if (disposing)
